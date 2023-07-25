afb88832f8f99ab348c85a4a3e062ad7a2eb2a7d
rreischke <rreischke@posteo.net>
2023-07-25 09:25:03 +0200

    update logeo

--- docs/conf.py
+++ docs/conf.py
@@ -53,6 +53,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_logo = 'Onecov_letter_logo.png'
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
