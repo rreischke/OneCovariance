resorting input for arbitrary covariance, more fixing of sonoma issue

input/arbitrary_summary/script_weights/get_weights_bandpowers.py
@@ -128,13 +128,13 @@ def get_gpm(ell_bins,ell_ul_bins, thetabins, T_of_theta, Norm, type):
             g_minus[i_ell,i_theta] = 1./(thetabins[i_theta]/60/180*np.pi)**2*(curly_G_minus_up - curly_G_minus_lo)
             h_ell[i_ell,i_theta] = - 1./(thetabins[i_theta]/60/180*np.pi)**2*(theta_times_ell_up*jv(1, theta_times_ell_up) - theta_times_ell_lo*jv(1, theta_times_ell_lo) + 2.*jv(0, theta_times_ell_up) - 2.*jv(0, theta_times_ell_lo))  
         if i_ell+1 < 10:
-            filename_mm = "./../real_weight_bandpowers_mmE_0" + str(i_ell+1) + ".table"
-            filename_gm = "./../real_weight_bandpowers_gm_0" + str(i_ell+1) + ".table"
-            filename_gg = "./../real_weight_bandpowers_gg_0" + str(i_ell+1) + ".table"
+            filename_mm = "./../bandpowers/real_weight_bandpowers_mmE_0" + str(i_ell+1) + ".table"
+            filename_gm = "./../bandpowers/real_weight_bandpowers_gm_0" + str(i_ell+1) + ".table"
+            filename_gg = "./../bandpowers/real_weight_bandpowers_gg_0" + str(i_ell+1) + ".table"
         else:
-            filename_mm = "./../real_weight_bandpowers_mmE_" + str(i_ell+1) + ".table"
-            filename_gm = "./../real_weight_bandpowers_gm_" + str(i_ell+1) + ".table"
-            filename_gg = "./../real_weight_bandpowers_gg_" + str(i_ell+1) + ".table"
+            filename_mm = "./../bandpowers/real_weight_bandpowers_mmE_" + str(i_ell+1) + ".table"
+            filename_gm = "./../bandpowers/real_weight_bandpowers_gm_" + str(i_ell+1) + ".table"
+            filename_gg = "./../bandpowers/real_weight_bandpowers_gg_" + str(i_ell+1) + ".table"
         if type == 'mm':
             np.savetxt(filename_mm,np.array([thetabins,g_plus[i_ell, :]*T_of_theta*np.pi/Norm[i_ell]]).T)
         if type == 'gg':
@@ -142,9 +142,9 @@ def get_gpm(ell_bins,ell_ul_bins, thetabins, T_of_theta, Norm, type):
         if type == 'gm':
             np.savetxt(filename_gm,np.array([thetabins,h_ell[i_ell, :]*T_of_theta*2*np.pi/Norm[i_ell]]).T)
         if i_ell+1 < 10:
-            filename_mm = "./../real_weight_bandpowers_mmB_0" + str(i_ell+1) + ".table"
+            filename_mm = "./../bandpowers/real_weight_bandpowers_mmB_0" + str(i_ell+1) + ".table"
         else:
-            filename_mm = "./../real_weight_bandpowers_mmB_" + str(i_ell+1) + ".table"
+            filename_mm = "./../bandpowers/real_weight_bandpowers_mmB_" + str(i_ell+1) + ".table"
         if type == 'mm':
             np.savetxt(filename_mm,np.array([thetabins,g_minus[i_ell, :]*T_of_theta*np.pi/Norm[i_ell]]).T)
         
@@ -240,13 +240,13 @@ def calc_fourier_filters_bp(ell_bins,ell_ul_bins, ell_fourier_integral, thetabin
                 'in ' + str(round(eta, 1)) + 'min', end="")
         tcomb += 1
         if i_ell+1 < 10:
-            filename_mm = "./../fourier_weight_bandpowers_mmE_0" + str(i_ell+1) + ".table"
-            filename_gm = "./../fourier_weight_bandpowers_gm_0" + str(i_ell+1) + ".table"
-            filename_gg = "./../fourier_weight_bandpowers_gg_0" + str(i_ell+1) + ".table"
+            filename_mm = "./../bandpowers/fourier_weight_bandpowers_mmE_0" + str(i_ell+1) + ".table"
+            filename_gm = "./../bandpowers/fourier_weight_bandpowers_gm_0" + str(i_ell+1) + ".table"
+            filename_gg = "./../bandpowers/fourier_weight_bandpowers_gg_0" + str(i_ell+1) + ".table"
         else:
-            filename_mm = "./../fourier_weight_bandpowers_mmE_" + str(i_ell+1) + ".table"
-            filename_gm = "./../fourier_weight_bandpowers_gm_" + str(i_ell+1) + ".table"
-            filename_gg = "./../fourier_weight_bandpowers_gg_" + str(i_ell+1) + "table"
+            filename_mm = "./../bandpowers/fourier_weight_bandpowers_mmE_" + str(i_ell+1) + ".table"
+            filename_gm = "./../bandpowers/fourier_weight_bandpowers_gm_" + str(i_ell+1) + ".table"
+            filename_gg = "./../bandpowers/fourier_weight_bandpowers_gg_" + str(i_ell+1) + "table"
         if type == "mm":
             np.savetxt(filename_mm,np.array([ell_fourier_integral,Wl_EE[i_ell, :]*np.pi/Norm[i_ell]]).T)
         if type == "gm":
@@ -255,9 +255,9 @@ def calc_fourier_filters_bp(ell_bins,ell_ul_bins, ell_fourier_integral, thetabin
             np.savetxt(filename_gg,np.array([ell_fourier_integral,Wl_EE[i_ell, :]*np.pi*2.0/Norm[i_ell]]).T)
         
         if i_ell+1 < 10:
-            filename_mm = "./../fourier_weight_bandpowers_mmB_0" + str(i_ell+1) + ".table"
+            filename_mm = "./../bandpowers/fourier_weight_bandpowers_mmB_0" + str(i_ell+1) + ".table"
         else:
-            filename_mm = "./../fourier_weight_bandpowers_mmB_" + str(i_ell+1) + ".table"
+            filename_mm = "./../bandpowers/fourier_weight_bandpowers_mmB_" + str(i_ell+1) + ".table"
         if type == "mm":
             np.savetxt(filename_mm,np.array([ell_fourier_integral,Wl_EB[i_ell, :]*np.pi/Norm[i_ell]]).T)
     print("")

input/arbitrary_summary/script_weights/get_weights_cosebis.py
@@ -215,13 +215,13 @@ for nn in range(1,Nmax_mm+1):
     tpn[:,0] /= arcmintorad
     tmn[:,0] /= arcmintorad
     if nn < 10:
-        file_tpn = "./../Tp_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
-        file_tmn = "./../Tm_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
-        file_Wn = "./../Wn_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
+        file_tpn = "./../cosebis/Tp_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
+        file_tmn = "./../cosebis/Tm_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
+        file_Wn = "./../cosebis/Wn_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
     else:
-        file_tpn = "./../Tp_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
-        file_tmn = "./../Tm_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
-        file_Wn = "./../Wn_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
+        file_tpn = "./../cosebis/Tp_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
+        file_tmn = "./../cosebis/Tm_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
+        file_Wn = "./../cosebis/Wn_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
     np.savetxt(file_tpn,tpn)
     np.savetxt(file_tmn,tmn)    
     if get_W_ell_as_well:
@@ -235,11 +235,11 @@ Qngm = get_Qn(Ungm, theta_gm, Nmax_gm)
 for nn in range(1,Nmax_gg+1):
     Wpsigg = get_Wpsi_ell(nn - 1, Ungg[nn-1,:], theta_gg)
     if nn < 10:
-        file_Ugg = "./../Ugg_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_0"+str(nn)  + ".table"
-        file_Wn = "./../Wn_psigg_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
+        file_Ugg = "./../cosebis/Ugg_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_0"+str(nn)  + ".table"
+        file_Wn = "./../cosebis/Wn_psigg_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
     else:
-        file_Ugg = "./../Ugg_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_"+str(nn)  + ".table"
-        file_Wn = "./../Wn_psigg"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
+        file_Ugg = "./../cosebis/Ugg_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_"+str(nn)  + ".table"
+        file_Wn = "./../cosebis/Wn_psigg"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
     np.savetxt(file_Ugg,np.array([theta_gg/arcmintorad,Ungg[nn-1,:]*arcmintorad**2]).T)
     if get_W_ell_as_well:
         np.savetxt(file_Wn, np.array([ell,Wpsigg]).T)
@@ -247,11 +247,11 @@ for nn in range(1,Nmax_gg+1):
 for nn in range(1,Nmax_gm+1):
     Wpsigm = get_Wpsi_ell(nn - 1, Ungm[nn-1,:], theta_gm)
     if nn < 10:
-        file_Qgm = "./../Qgm_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_0"+str(nn)  + ".table"
-        file_Wn = "./../Wn_psigm_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
+        file_Qgm = "./../cosebis/Qgm_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_0"+str(nn)  + ".table"
+        file_Wn = "./../cosebis/Wn_psigm_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
     else:
-        file_Qgm = "./../Qgm_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_"+str(nn)  + ".table"
-        file_Wn = "./../Wn_psigm"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
+        file_Qgm = "./../cosebis/Qgm_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_"+str(nn)  + ".table"
+        file_Wn = "./../cosebis/Wn_psigm"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
     np.savetxt(file_Qgm,np.array([theta_gm/arcmintorad,Qngm[nn-1,:]*arcmintorad**2]).T)
     if get_W_ell_as_well:
         np.savetxt(file_Wn, np.array([ell,Wpsigm]).T)

input/arbitrary_summary/script_weights/get_weights_cosebis_dimensionless.py
@@ -181,13 +181,13 @@ for nn in range(1,Nmax_mm+1):
     tpn[:,0] /= arcmintorad
     tmn[:,0] /= arcmintorad
     if nn < 10:
-        file_tpn = "./../dimensionless_Tp_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
-        file_tmn = "./../dimensionless_Tm_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
-        file_Wn = "./../dimensionless_Wn_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
+        file_tpn = "./../dimless_cosebis/dimensionless_Tp_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
+        file_tmn = "./../dimless_cosebis/dimensionless_Tm_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
+        file_Wn = "./../dimless_cosebis/dimensionless_Wn_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
     else:
-        file_tpn = "./../dimensionless_Tp_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
-        file_tmn = "./../dimensionless_Tm_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
-        file_Wn = "./../dimensionless_Wn_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
+        file_tpn = "./../dimless_cosebis/dimensionless_Tp_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
+        file_tmn = "./../dimless_cosebis/dimensionless_Tm_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
+        file_Wn = "./../dimless_cosebis/dimensionless_Wn_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
     np.savetxt(file_tpn,tpn)
     np.savetxt(file_tmn,tmn)    
     if get_W_ell_as_well:

input/arbitrary_summary/script_weights/get_weights_realspace.py
@@ -64,15 +64,15 @@ for i_theta in range(len(theta_bins)):
     theta_l = theta_ul_bins[i_theta]/60/180*np.pi
     K_gg = 2/(theta_u**2 - theta_l**2)/fourier_ell*(theta_u*jv(1,theta_u*fourier_ell) - theta_l*jv(1,theta_l*fourier_ell))
     if i_theta+1 < 10:
-        filename_gg = "./../fourier_weight_realspace_cf_gg_0" + str(i_theta+1) + ".table"
+        filename_gg = "./../rcf/fourier_weight_realspace_cf_gg_0" + str(i_theta+1) + ".table"
     else:
-        filename_gg = "./../fourier_weight_realspace_cf_gg_" + str(i_theta+1) + ".table"
+        filename_gg = "./../rcf/fourier_weight_realspace_cf_gg_" + str(i_theta+1) + ".table"
     np.savetxt(filename_gg,np.array([fourier_ell,K_gg]).T)
     R = top_hat(real_theta, theta_bins[i_theta] - theta_ul_bins[i_theta], theta_ul_bins[i_theta+1] - theta_bins[i_theta],  theta_bins[i_theta])/(real_theta)* (60*180/np.pi)**2/np.sqrt(2)
     if i_theta+1 < 10:
-        filename = "./../real_weight_realspace_cf_gg_0" + str(i_theta+1) + ".table"
+        filename = "./../rcf/real_weight_realspace_cf_gg_0" + str(i_theta+1) + ".table"
     else:
-        filename = "./../real_weight_realspace_cf_gg_" + str(i_theta+1) + ".table"
+        filename = "./../rcf/real_weight_realspace_cf_gg_" + str(i_theta+1) + ".table"
     np.savetxt(filename,np.array([real_theta,R]).T)
 
 # Define theta-range for gamma_t (i.e. gm)
@@ -87,15 +87,15 @@ for i_theta in range(len(theta_bins)):
      
     K_gm = 2/(xu**2 - xl**2)*(-xu*jv(1,xu) + xl*jv(1,xl) -2*jv(0,xu) + 2*jv(0,xl))
     if i_theta+1 < 10:
-        filename_gm = "./../fourier_weight_realspace_cf_gm_0" + str(i_theta+1) + ".table"
+        filename_gm = "./../rcf/fourier_weight_realspace_cf_gm_0" + str(i_theta+1) + ".table"
     else:
-        filename_gm = "./../fourier_weight_realspace_cf_gm_" + str(i_theta+1) + ".table"
+        filename_gm = "./../rcf/fourier_weight_realspace_cf_gm_" + str(i_theta+1) + ".table"
     np.savetxt(filename_gm,np.array([fourier_ell,K_gm]).T)
     R = top_hat(real_theta, theta_bins[i_theta] - theta_ul_bins[i_theta], theta_ul_bins[i_theta+1] - theta_bins[i_theta],  theta_bins[i_theta])/(real_theta)* (60*180/np.pi)**2/np.sqrt(2)
     if i_theta+1 < 10:
-        filename = "./../real_weight_realspace_cf_gm_0" + str(i_theta+1) + ".table"
+        filename = "./../rcf/real_weight_realspace_cf_gm_0" + str(i_theta+1) + ".table"
     else:
-        filename = "./../real_weight_realspace_cf_gm_" + str(i_theta+1) + ".table"
+        filename = "./../rcf/real_weight_realspace_cf_gm_" + str(i_theta+1) + ".table"
     np.savetxt(filename,np.array([real_theta,R]).T)
 
 # Define theta-range for xip_m (i.e. mm)
@@ -110,26 +110,26 @@ for i_theta in range(len(theta_bins)):
     xl = fourier_ell*theta_l
     K_mm = 2/(xu**2 - xl**2)*(xu*jv(1,xu) - xl*jv(1,xl))
     if i_theta+1 < 10:
-        filename_mm = "./../fourier_weight_realspace_cf_mm_p_0" + str(i_theta+1) + ".table"
+        filename_mm = "./../rcf/fourier_weight_realspace_cf_mm_p_0" + str(i_theta+1) + ".table"
     else:
-        filename_mm = "./../fourier_weight_realspace_cf_mm_p_" + str(i_theta+1) + ".table"
+        filename_mm = "./../rcf/fourier_weight_realspace_cf_mm_p_" + str(i_theta+1) + ".table"
     np.savetxt(filename_mm,np.array([fourier_ell,K_mm]).T)
     K_mm = 2/(xu**2 - xl**2)*((xu - 8/xu)*jv(1,xu) - 8*jv(2,xu) - (xl-8/xl)*jv(1,xl) + 8*jv(2,xl))
     if i_theta+1 < 10:
-        filename_mm = "./../fourier_weight_realspace_cf_mm_m_0" + str(i_theta+1) + ".table"
+        filename_mm = "./../rcf/fourier_weight_realspace_cf_mm_m_0" + str(i_theta+1) + ".table"
     else:
-        filename_mm = "./../fourier_weight_realspace_cf_mm_m_" + str(i_theta+1) + ".table"
+        filename_mm = "./../rcf/fourier_weight_realspace_cf_mm_m_" + str(i_theta+1) + ".table"
     np.savetxt(filename_mm,np.array([fourier_ell,K_mm]).T)
     R = top_hat(real_theta, theta_bins[i_theta] - theta_ul_bins[i_theta], theta_ul_bins[i_theta+1] - theta_bins[i_theta],  theta_bins[i_theta])/(real_theta)* (60*180/np.pi)**2/np.sqrt(2)
     if i_theta+1 < 10:
-        filename = "./../real_weight_realspace_cf_mm_p_0" + str(i_theta+1) + ".table"
+        filename = "./../rcf/real_weight_realspace_cf_mm_p_0" + str(i_theta+1) + ".table"
     else:
-        filename = "./../real_weight_realspace_cf_mm_p_" + str(i_theta+1) + ".table"
+        filename = "./../rcf/real_weight_realspace_cf_mm_p_" + str(i_theta+1) + ".table"
     np.savetxt(filename,np.array([real_theta,R]).T)
     if i_theta+1 < 10:
-        filename = "./../real_weight_realspace_cf_mm_m_0" + str(i_theta+1) + ".table"
+        filename = "./../rcf/real_weight_realspace_cf_mm_m_0" + str(i_theta+1) + ".table"
     else:
-        filename = "./../real_weight_realspace_cf_mm_m_" + str(i_theta+1) + ".table"
+        filename = "./../rcf/real_weight_realspace_cf_mm_m_" + str(i_theta+1) + ".table"
     np.savetxt(filename,np.array([real_theta,R]).T)
 
 

onecov/cov_polyspectra.py
@@ -1681,6 +1681,14 @@ class PolySpectra(HaloModel):
         """
         return 5/7 + 2/7 * mu*mu + .5*mu * (ki/kj + kj/ki)
 
+    def calc_int_for_trispec_4h(self,
+                                  phi,
+                                  ki,
+                                  kj):
+        return self.__calc_int_for_trispec_4h(phi,
+                                  ki,
+                                  kj)
+    
     def __calc_int_for_trispec_4h(self,
                                   phi,
                                   ki,
@@ -1702,29 +1710,29 @@ class PolySpectra(HaloModel):
 
         pm = 0
         F2_im, F2_jm = 0, 0
-        #if not ((np.fabs(ki-kj) < self.trispec_matter_klim)
-        #        and (1-mu < self.trispec_matter_mulim)):
         km = np.sqrt(ki*ki + kj*kj - 2*ki*kj*mu)
         if km == 0:
-            km = 1e-20
-        mu_im = kj/km * mu - ki/km
-        mu_jm = kj/km - mu * ki/km
-        pm = np.exp(self.Plin_spline(np.log(km)))
-        F2_im = self.__pt_kernel_f2(mu_im, ki, km)
-        F2_jm = self.__pt_kernel_f2(-mu_jm, kj, km)
+            F2_im = 13./28
+            F2_jm = 13./28
+        else:
+            mu_im = kj/km * mu - ki/km
+            mu_jm = kj/km - mu * ki/km
+            pm = np.exp(self.Plin_spline(np.log(km)))
+            F2_im = self.__pt_kernel_f2(mu_im, ki, km)
+            F2_jm = self.__pt_kernel_f2(-mu_jm, kj, km)
 
         pp = 0
         F2_ip, F2_jp = 0, 0
-        #if not ((np.fabs(ki-kj) < self.trispec_matter_klim)
-        #        and (1+mu < self.trispec_matter_mulim)):
         kp = np.sqrt(ki*ki + kj*kj + 2*ki*kj*mu)
         if kp == 0:
-            kp = 1e-20
-        mu_ip = kj/kp * mu + ki/kp
-        mu_jp = ki/kp * mu + kj/kp
-        pp = np.exp(self.Plin_spline(np.log(kp)))
-        F2_ip = self.__pt_kernel_f2(-mu_ip, ki, kp)
-        F2_jp = self.__pt_kernel_f2(-mu_jp, kj, kp)
+            F2_ip = 13./28
+            F2_jp = 13./28
+        else:
+            mu_ip = kj/kp * mu + ki/kp
+            mu_jp = ki/kp * mu + kj/kp
+            pp = np.exp(self.Plin_spline(np.log(kp)))
+            F2_ip = self.__pt_kernel_f2(-mu_ip, ki, kp)
+            F2_jp = self.__pt_kernel_f2(-mu_jp, kj, kp)
 
         pi = np.exp(self.Plin_spline(np.log(ki)))
         pj = np.exp(self.Plin_spline(np.log(kj)))
@@ -1937,8 +1945,8 @@ class PolySpectra(HaloModel):
                             self.int_3h[i,j] = 2/np.pi * simpson(integrand, x = phis)
 
                         try:
-                            self.int_4h[i,j] = 2/np.pi \
-                                * quad(self.__calc_int_for_trispec_4h, 0, .5*np.pi,
+                            self.int_4h[i,j] = 1/np.pi \
+                                * quad(self.__calc_int_for_trispec_4h, 0, np.pi,
                                         args=(ki, kj), limit=100, epsrel=1e-05)[0]    
                         except IntegrationWarning:
                             integrand = np.zeros_like(phis)