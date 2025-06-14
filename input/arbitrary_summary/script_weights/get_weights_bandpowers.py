updated weight function scripts, fixed bug in the arbitrary covariance 4pt terms

input/arbitrary_summary/script_weights/get_weights_bandpowers.py
@@ -66,7 +66,87 @@ def main():
     L_bins_gg = args.L_bins_gg
     L_type_gg = args.L_type_gg
 
+    hdr_str_mm_plus_fourier = 'Bandpower weights for C_E in Fourier space\n'
+    hdr_str_mm_plus_fourier += 'lowest theta boundary = ' + str(theta_lo_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'highest theta boundary = ' + str(theta_up_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'apodization beand width = ' + str(delta_ln_theta_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'minimum multipole = ' + str(L_min_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'maximum multipole = ' + str(L_max_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'number of bandpowers modes = ' + str(L_bins_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'binning type for multipoles = ' + str(L_type_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'ell      W(ell)'
 
+    hdr_str_mm_minus_fourier = 'Bandpower weights for C_B in Fourier space\n'
+    hdr_str_mm_minus_fourier += 'lowest theta boundary = ' + str(theta_lo_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'highest theta boundary = ' + str(theta_up_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'apodization beand width = ' + str(delta_ln_theta_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'minimum multipole = ' + str(L_min_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'maximum multipole = ' + str(L_max_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'number of bandpowers modes = ' + str(L_bins_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'binning type for multipoles = ' + str(L_type_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'ell      W(ell)'
+
+
+    hdr_str_mm_plus_real = 'Bandpower weights for C_E in Fourier space\n'
+    hdr_str_mm_plus_real += 'lowest theta boundary = ' + str(theta_lo_mm) + '\n'
+    hdr_str_mm_plus_real += 'highest theta boundary = ' + str(theta_up_mm) + '\n'
+    hdr_str_mm_plus_real += 'apodization beand width = ' + str(delta_ln_theta_mm) + '\n'
+    hdr_str_mm_plus_real += 'minimum multipole = ' + str(L_min_mm) + '\n'
+    hdr_str_mm_plus_real += 'maximum multipole = ' + str(L_max_mm) + '\n'
+    hdr_str_mm_plus_real += 'number of bandpowers modes = ' + str(L_bins_mm) + '\n'
+    hdr_str_mm_plus_real += 'binning type for multipoles = ' + str(L_type_mm) + '\n'
+    hdr_str_mm_plus_real += 'theta[arcmin]      R(theta)'
+
+    hdr_str_mm_minus_real = 'Bandpower weights for C_B in Fourier space\n'
+    hdr_str_mm_minus_real += 'lowest theta boundary = ' + str(theta_lo_mm) + '\n'
+    hdr_str_mm_minus_real += 'highest theta boundary = ' + str(theta_up_mm) + '\n'
+    hdr_str_mm_minus_real += 'apodization beand width = ' + str(delta_ln_theta_mm) + '\n'
+    hdr_str_mm_minus_real += 'minimum multipole = ' + str(L_min_mm) + '\n'
+    hdr_str_mm_minus_real += 'maximum multipole = ' + str(L_max_mm) + '\n'
+    hdr_str_mm_minus_real += 'number of bandpowers modes = ' + str(L_bins_mm) + '\n'
+    hdr_str_mm_minus_real += 'binning type for multipoles = ' + str(L_type_mm) + '\n'
+    hdr_str_mm_minus_real += 'theta[arcmin]      R(theta)'
+
+
+    hdr_str_gm_fourier = 'Bandpower weights for C of ggl in Fourier space\n'
+    hdr_str_gm_fourier += 'lowest theta boundary = ' + str(theta_lo_gm) + '\n'
+    hdr_str_gm_fourier += 'highest theta boundary = ' + str(theta_up_gm) + '\n'
+    hdr_str_gm_fourier += 'apodization beand width = ' + str(delta_ln_theta_gm) + '\n'
+    hdr_str_gm_fourier += 'minimum multipole = ' + str(L_min_gm) + '\n'
+    hdr_str_gm_fourier += 'maximum multipole = ' + str(L_max_gm) + '\n'
+    hdr_str_gm_fourier += 'number of bandpowers modes = ' + str(L_bins_gm) + '\n'
+    hdr_str_gm_fourier += 'binning type for multipoles = ' + str(L_type_gm) + '\n'
+    hdr_str_gm_fourier += 'ell      W(ell)'
+
+    hdr_str_gm_real = 'Bandpower weights for C of ggl in Fourier space\n'
+    hdr_str_gm_real += 'lowest theta boundary = ' + str(theta_lo_gm) + '\n'
+    hdr_str_gm_real += 'highest theta boundary = ' + str(theta_up_gm) + '\n'
+    hdr_str_gm_real += 'apodization beand width = ' + str(delta_ln_theta_gm) + '\n'
+    hdr_str_gm_real += 'minimum multipole = ' + str(L_min_gm) + '\n'
+    hdr_str_gm_real += 'maximum multipole = ' + str(L_max_gm) + '\n'
+    hdr_str_gm_real += 'number of bandpowers modes = ' + str(L_bins_gm) + '\n'
+    hdr_str_gm_real += 'binning type for multipoles = ' + str(L_type_gm) + '\n'
+    hdr_str_gm_real += 'theta[arcmin]      R(theta)'
+
+    hdr_str_gg_fourier = 'Bandpower weights for C of clustering in Fourier space\n'
+    hdr_str_gg_fourier += 'lowest theta boundary = ' + str(theta_lo_gg) + '\n'
+    hdr_str_gg_fourier += 'highest theta boundary = ' + str(theta_up_gg) + '\n'
+    hdr_str_gg_fourier += 'apodization beand width = ' + str(delta_ln_theta_gg) + '\n'
+    hdr_str_gg_fourier += 'minimum multipole = ' + str(L_min_gg) + '\n'
+    hdr_str_gg_fourier += 'maximum multipole = ' + str(L_max_gg) + '\n'
+    hdr_str_gg_fourier += 'number of bandpowers modes = ' + str(L_bins_gg) + '\n'
+    hdr_str_gg_fourier += 'binning type for multipoles = ' + str(L_type_gg) + '\n'
+    hdr_str_gg_fourier += 'ell      W(ell)'
+
+    hdr_str_gg_real = 'Bandpower weights for C of clustering Fourier space\n'
+    hdr_str_gg_real += 'lowest theta boundary = ' + str(theta_lo_gg) + '\n'
+    hdr_str_gg_real += 'highest theta boundary = ' + str(theta_up_gg) + '\n'
+    hdr_str_gg_real += 'apodization beand width = ' + str(delta_ln_theta_gg) + '\n'
+    hdr_str_gg_real += 'minimum multipole = ' + str(L_min_gg) + '\n'
+    hdr_str_gg_real += 'maximum multipole = ' + str(L_max_gg) + '\n'
+    hdr_str_gg_real += 'number of bandpowers modes = ' + str(L_bins_gg) + '\n'
+    hdr_str_gg_real += 'binning type for multipoles = ' + str(L_type_gg) + '\n'
+    hdr_str_gg_real += 'theta[arcmin]      R(theta)'
 
     theta_min_gm = np.exp(np.log(theta_lo_gm) - delta_ln_theta_gm/2) # real lower limit after apodisation
     theta_max_gm = np.exp(np.log(theta_up_gm) + delta_ln_theta_gm/2) # real upper limit after apodisation
@@ -166,17 +246,17 @@ def main():
                 filename_gm = "./../bandpowers/real_weight_bandpowers_gm_" + str(i_ell+1) + ".table"
                 filename_gg = "./../bandpowers/real_weight_bandpowers_gg_" + str(i_ell+1) + ".table"
             if type == 'mm':
-                np.savetxt(filename_mm,np.array([thetabins,g_plus[i_ell, :]*T_of_theta*np.pi/Norm[i_ell]]).T)
+                np.savetxt(filename_mm,np.array([thetabins,g_plus[i_ell, :]*T_of_theta*np.pi/Norm[i_ell]]).T, header=hdr_str_mm_plus_real)
             if type == 'gg':
-                np.savetxt(filename_gg,np.array([thetabins,g_plus[i_ell, :]*T_of_theta*np.pi/Norm[i_ell]]).T)
+                np.savetxt(filename_gg,np.array([thetabins,g_plus[i_ell, :]*T_of_theta*np.pi/Norm[i_ell]]).T, header=hdr_str_gg_real)
             if type == 'gm':
-                np.savetxt(filename_gm,np.array([thetabins,h_ell[i_ell, :]*T_of_theta*2*np.pi/Norm[i_ell]]).T)
+                np.savetxt(filename_gm,np.array([thetabins,h_ell[i_ell, :]*T_of_theta*2*np.pi/Norm[i_ell]]).T, header=hdr_str_gm_real)
             if i_ell+1 < 10:
                 filename_mm = "./../bandpowers/real_weight_bandpowers_mmB_0" + str(i_ell+1) + ".table"
             else:
                 filename_mm = "./../bandpowers/real_weight_bandpowers_mmB_" + str(i_ell+1) + ".table"
             if type == 'mm':
-                np.savetxt(filename_mm,np.array([thetabins,g_minus[i_ell, :]*T_of_theta*np.pi/Norm[i_ell]]).T)
+                np.savetxt(filename_mm,np.array([thetabins,g_minus[i_ell, :]*T_of_theta*np.pi/Norm[i_ell]]).T, header=hdr_str_mm_minus_real)
             
         
         return g_plus, g_minus, h_ell
@@ -278,18 +358,18 @@ def main():
                 filename_gm = "./../bandpowers/fourier_weight_bandpowers_gm_" + str(i_ell+1) + ".table"
                 filename_gg = "./../bandpowers/fourier_weight_bandpowers_gg_" + str(i_ell+1) + "table"
             if type == "mm":
-                np.savetxt(filename_mm,np.array([ell_fourier_integral,Wl_EE[i_ell, :]*np.pi/Norm[i_ell]]).T)
+                np.savetxt(filename_mm,np.array([ell_fourier_integral,Wl_EE[i_ell, :]*np.pi/Norm[i_ell]]).T, header=hdr_str_mm_plus_fourier)
             if type == "gm":
-                np.savetxt(filename_gm,np.array([ell_fourier_integral,Wl_nE[i_ell, :]*np.pi*2.0/Norm[i_ell]]).T)
+                np.savetxt(filename_gm,np.array([ell_fourier_integral,Wl_nE[i_ell, :]*np.pi*2.0/Norm[i_ell]]).T, header=hdr_str_gm_fourier)
             if type == "gg":
-                np.savetxt(filename_gg,np.array([ell_fourier_integral,Wl_EE[i_ell, :]*np.pi*2.0/Norm[i_ell]]).T)
+                np.savetxt(filename_gg,np.array([ell_fourier_integral,Wl_EE[i_ell, :]*np.pi*2.0/Norm[i_ell]]).T, header=hdr_str_gg_fourier)
             
             if i_ell+1 < 10:
                 filename_mm = "./../bandpowers/fourier_weight_bandpowers_mmB_0" + str(i_ell+1) + ".table"
             else:
                 filename_mm = "./../bandpowers/fourier_weight_bandpowers_mmB_" + str(i_ell+1) + ".table"
             if type == "mm":
-                np.savetxt(filename_mm,np.array([ell_fourier_integral,Wl_EB[i_ell, :]*np.pi/Norm[i_ell]]).T)
+                np.savetxt(filename_mm,np.array([ell_fourier_integral,Wl_EB[i_ell, :]*np.pi/Norm[i_ell]]).T,  header=hdr_str_mm_minus_fourier)
         print("")
         
 

input/arbitrary_summary/script_weights/get_weights_cosebis.py
@@ -59,6 +59,59 @@ def main():
     tmin_gg = args.tmin_gg
     tmax_gg = args.tmax_gg
 
+    hdr_str_mm_plus_fourier = 'COSEBI weights for En in Fourier space\n'
+    hdr_str_mm_plus_fourier += 'lowest theta boundary = ' + str(tmin_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'highest theta boundary = ' + str(tmax_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'number of COSEBI modes = ' + str(Nmax_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'ell      W(ell)'
+
+    hdr_str_mm_minus_fourier = 'COSEBI weights for Bn in Fourier space\n'
+    hdr_str_mm_minus_fourier += 'lowest theta boundary = ' + str(tmin_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'highest theta boundary = ' + str(tmax_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'number of COSEBI modes = ' + str(Nmax_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'ell      W(ell)'
+
+
+    hdr_str_mm_plus_real = 'COSEBI weights for En in Real space\n'
+    hdr_str_mm_plus_real += 'lowest theta boundary = ' + str(tmin_mm) + '\n'
+    hdr_str_mm_plus_real += 'highest theta boundary = ' + str(tmin_mm) + '\n'
+    hdr_str_mm_plus_real += 'number of COSEBI modes = ' + str(Nmax_mm) + '\n'
+    hdr_str_mm_plus_real += 'theta[arcmin]      T_+(theta)'
+
+    hdr_str_mm_minus_real = 'COSEBI weights for En in Real space\n'
+    hdr_str_mm_minus_real += 'lowest theta boundary = ' + str(tmin_mm) + '\n'
+    hdr_str_mm_minus_real += 'highest theta boundary = ' + str(tmax_mm) + '\n'
+    hdr_str_mm_minus_real += 'number of COSEBI modes = ' + str(Nmax_mm) + '\n'
+    hdr_str_mm_minus_real += 'theta[arcmin]      T_-(theta)'
+
+
+    
+    hdr_str_gm_real = 'Psi stats weights for ggl in Real space\n'
+    hdr_str_gm_real += 'lowest theta boundary = ' + str(tmin_gm) + '\n'
+    hdr_str_gm_real += 'highest theta boundary = ' + str(tmax_gm) + '\n'
+    hdr_str_gm_real += 'number of COSEBI modes = ' + str(Nmax_gm) + '\n'
+    hdr_str_gm_real += 'theta[arcmin]      Q(theta)'
+
+    hdr_str_gm_fourier = 'Psi stats weights for ggl in Fourier space\n'
+    hdr_str_gm_fourier += 'lowest theta boundary = ' + str(tmin_gm) + '\n'
+    hdr_str_gm_fourier += 'highest theta boundary = ' + str(tmax_gm) + '\n'
+    hdr_str_gm_fourier += 'number of COSEBI modes = ' + str(Nmax_gm) + '\n'
+    hdr_str_gm_fourier += 'ell      W(ell)'
+
+
+    hdr_str_gg_real = 'Psi stats weights for clustering in Real space\n'
+    hdr_str_gg_real += 'lowest theta boundary = ' + str(tmin_gg) + '\n'
+    hdr_str_gg_real += 'highest theta boundary = ' + str(tmax_gg) + '\n'
+    hdr_str_gg_real += 'number of COSEBI modes = ' + str(Nmax_gg) + '\n'
+    hdr_str_gg_real += 'theta[arcmin]      U(theta)'
+
+    hdr_str_gg_fourier = 'Psi stats weights for clustering in Fourier space\n'
+    hdr_str_gg_fourier += 'lowest theta boundary = ' + str(tmin_gg) + '\n'
+    hdr_str_gg_fourier += 'highest theta boundary = ' + str(tmax_gg) + '\n'
+    hdr_str_gg_fourier += 'number of COSEBI modes = ' + str(Nmax_gg) + '\n'
+    hdr_str_gg_fourier += 'ell      W(ell)'
+
+
 
     tmin_mm *= arcmintorad
     tmax_mm *= arcmintorad
@@ -70,6 +123,7 @@ def main():
     theta_mm = np.geomspace(tmin_mm,tmax_mm, N_theta)
     theta_gm = np.geomspace(tmin_gm,tmax_gm, N_theta)
 
+    
 
 
     #####################
@@ -217,7 +271,7 @@ def main():
         theta_integral_weight = np.ones((len(theta),len(theta)))
         theta_integral_weight = np.triu(theta_integral_weight)[None, :, :]*np.ones(Nmax)[:, None, None]
         theta_integral_weight += (np.diag(np.ones_like(theta)))[None,:,:]
-        return 2/theta**2*integrate.simpson(theta_integral_weight*theta[None,:,None]*Un[:,:,None],theta,axis = 1) - Un
+        return 2/theta**2*integrate.simpson(theta_integral_weight*theta[None,:,None]*Un[:,:,None], x=theta,axis = 1) - Un
 
     def get_Wpsi_ell(n, Un, theta):
         lev = levin.Levin(0, 16, 32, 1e-8, 200, num_cores)
@@ -245,10 +299,10 @@ def main():
             file_tpn = "./../cosebis/Tp_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
             file_tmn = "./../cosebis/Tm_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
             file_Wn = "./../cosebis/Wn_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
-        np.savetxt(file_tpn,tpn)
-        np.savetxt(file_tmn,tmn)    
+        np.savetxt(file_tpn,tpn,header=hdr_str_mm_plus_real)
+        np.savetxt(file_tmn,tmn,header=hdr_str_mm_minus_real)    
         if get_W_ell_as_well:
-            np.savetxt(file_Wn, np.array([ell,result_Well]).T)
+            np.savetxt(file_Wn, np.array([ell,result_Well]).T, header=hdr_str_mm_plus_fourier)
         
 
     Ungg = get_Un(tmax_gg, tmin_gg, theta_gg, Nmax_gg)
@@ -263,9 +317,9 @@ def main():
         else:
             file_Ugg = "./../cosebis/Ugg_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_"+str(nn)  + ".table"
             file_Wn = "./../cosebis/Wn_psigg"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
-        np.savetxt(file_Ugg,np.array([theta_gg/arcmintorad,Ungg[nn-1,:]*arcmintorad**2]).T)
+        np.savetxt(file_Ugg,np.array([theta_gg/arcmintorad,Ungg[nn-1,:]*arcmintorad**2]).T, header=hdr_str_gg_real)
         if get_W_ell_as_well:
-            np.savetxt(file_Wn, np.array([ell,Wpsigg]).T)
+            np.savetxt(file_Wn, np.array([ell,Wpsigg]).T, header=hdr_str_gg_fourier)
 
     for nn in range(1,Nmax_gm+1):
         Wpsigm = get_Wpsi_ell(nn - 1, Ungm[nn-1,:], theta_gm)
@@ -275,9 +329,9 @@ def main():
         else:
             file_Qgm = "./../cosebis/Qgm_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_"+str(nn)  + ".table"
             file_Wn = "./../cosebis/Wn_psigm"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
-        np.savetxt(file_Qgm,np.array([theta_gm/arcmintorad,Qngm[nn-1,:]*arcmintorad**2]).T)
+        np.savetxt(file_Qgm,np.array([theta_gm/arcmintorad,Qngm[nn-1,:]*arcmintorad**2]).T, header=hdr_str_gm_real)
         if get_W_ell_as_well:
-            np.savetxt(file_Wn, np.array([ell,Wpsigm]).T)
+            np.savetxt(file_Wn, np.array([ell,Wpsigm]).T, header=hdr_str_gm_fourier)
 
 if __name__ == '__main__':
     main()

input/arbitrary_summary/script_weights/get_weights_cosebis_dimensionless.py
@@ -32,11 +32,36 @@ def main():
 
     ell_min = 1 # Minimum multipole
     ell_max = 1e5 # Maximum multipole
-    N_ell = args.nf # Number of Fourier modes
-    N_theta = args.nt
+    N_ell = args.nfourier # Number of Fourier modes
+    N_theta = args.ntheta
     get_W_ell_as_well = True # If true the Well are calculated
     num_cores = args.nthread #number of cores used
 
+
+    hdr_str_mm_plus_fourier = 'dimensionless COSEBI weights for En in Fourier space\n'
+    hdr_str_mm_plus_fourier += 'lowest theta boundary = ' + str(tmin_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'highest theta boundary = ' + str(tmax_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'number of COSEBI modes = ' + str(Nmax_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'ell      W(ell)'
+
+    hdr_str_mm_minus_fourier = 'dimensionless COSEBI weights for Bn in Fourier space\n'
+    hdr_str_mm_minus_fourier += 'lowest theta boundary = ' + str(tmin_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'highest theta boundary = ' + str(tmax_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'number of COSEBI modes = ' + str(Nmax_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'ell      W(ell)'
+
+
+    hdr_str_mm_plus_real = 'dimensionless COSEBI weights for En in Real space\n'
+    hdr_str_mm_plus_real += 'lowest theta boundary = ' + str(tmin_mm) + '\n'
+    hdr_str_mm_plus_real += 'highest theta boundary = ' + str(tmin_mm) + '\n'
+    hdr_str_mm_plus_real += 'number of COSEBI modes = ' + str(Nmax_mm) + '\n'
+    hdr_str_mm_plus_real += 'theta[arcmin]      T_+(theta)'
+
+    hdr_str_mm_minus_real = 'dimensionless COSEBI weights for En in Real space\n'
+    hdr_str_mm_minus_real += 'lowest theta boundary = ' + str(tmin_mm) + '\n'
+    hdr_str_mm_minus_real += 'highest theta boundary = ' + str(tmax_mm) + '\n'
+    hdr_str_mm_minus_real += 'number of COSEBI modes = ' + str(Nmax_mm) + '\n'
+    hdr_str_mm_minus_real += 'theta[arcmin]      T_-(theta)'
 
     mp.dps = 160
     arcmintorad = 1./60./180.*np.pi
@@ -48,7 +73,6 @@ def main():
     tmin_mm *= arcmintorad
     tmax_mm *= arcmintorad
     theta_mm = np.geomspace(tmin_mm,tmax_mm, N_theta)
-
 
 
     #####################
@@ -205,10 +229,10 @@ def main():
             file_tpn = "./../dimless_cosebis/dimensionless_Tp_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
             file_tmn = "./../dimless_cosebis/dimensionless_Tm_" +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
             file_Wn = "./../dimless_cosebis/dimensionless_Wn_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
-        np.savetxt(file_tpn,tpn)
-        np.savetxt(file_tmn,tmn)    
+        np.savetxt(file_tpn,tpn, header=hdr_str_mm_plus_real)
+        np.savetxt(file_tmn,tmn, header=hdr_str_mm_minus_real)    
         if get_W_ell_as_well:
-            np.savetxt(file_Wn, np.array([ell,result_Well]).T)
+            np.savetxt(file_Wn, np.array([ell,result_Well]).T, header=hdr_str_mm_plus_fourier)
 
 if __name__ == '__main__':
     main()

input/arbitrary_summary/script_weights/get_weights_realspace.py
@@ -44,6 +44,69 @@ def main():
     theta_max_mm = args.theta_up_mm 
     ntheta_bins_mm = args.t_bins_mm
 
+
+    hdr_str_mm_plus_fourier = 'real space correlation function weights for Xi_+ in Fourier space\n'
+    hdr_str_mm_plus_fourier += 'lowest theta boundary = ' + str(theta_min_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'highest theta boundary = ' + str(theta_max_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'number of theta bins = ' + str(ntheta_bins_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'type of binning = ' + str(theta_type_mm) + '\n'
+    hdr_str_mm_plus_fourier += 'ell      W(ell)'
+
+    hdr_str_mm_minus_fourier = 'real space correlation function weights for Xi_- in Fourier space\n'
+    hdr_str_mm_minus_fourier += 'lowest theta boundary = ' + str(theta_min_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'highest theta boundary = ' + str(theta_max_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'number of theta bins = ' + str(ntheta_bins_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'type of binning = ' + str(theta_type_mm) + '\n'
+    hdr_str_mm_minus_fourier += 'ell      W(ell)'
+
+
+    hdr_str_mm_plus_real = 'real space correlation function weights for Xi_+ in Real space\n'
+    hdr_str_mm_plus_real += 'lowest theta boundary = ' + str(theta_min_mm) + '\n'
+    hdr_str_mm_plus_real += 'highest theta boundary = ' + str(theta_max_mm) + '\n'
+    hdr_str_mm_plus_real += 'number of theta bins = ' + str(ntheta_bins_mm) + '\n'
+    hdr_str_mm_plus_real += 'type of binning = ' + str(theta_type_mm) + '\n'
+    hdr_str_mm_plus_real += 'theta[arcmin]      R(theta)'
+
+
+    hdr_str_mm_minus_real = 'real space correlation function weights for Xi_- in Real space\n'
+    hdr_str_mm_minus_real += 'lowest theta boundary = ' + str(theta_min_mm) + '\n'
+    hdr_str_mm_minus_real += 'highest theta boundary = ' + str(theta_max_mm) + '\n'
+    hdr_str_mm_minus_real += 'number of theta bins = ' + str(ntheta_bins_mm) + '\n'
+    hdr_str_mm_minus_real += 'type of binning = ' + str(theta_type_mm) + '\n'
+    hdr_str_mm_minus_real += 'theta[arcmin]      R(theta)'
+
+
+    
+    hdr_str_gm_real = 'real space correlation function weights for gamma_t in Real space\n'
+    hdr_str_gm_real += 'lowest theta boundary = ' + str(theta_min_gm) + '\n'
+    hdr_str_gm_real += 'highest theta boundary = ' + str(theta_max_gm) + '\n'
+    hdr_str_gm_real += 'number of theta bins = ' + str(ntheta_bins_gm) + '\n'
+    hdr_str_gm_real += 'type of binning = ' + str(theta_type_gm) + '\n'
+    hdr_str_gm_real += 'theta[arcmin]      R(theta)'
+
+    hdr_str_gm_fourier = 'real space correlation function weights for gamma_t in Fourier space\n'
+    hdr_str_gm_fourier += 'lowest theta boundary = ' + str(theta_min_gm) + '\n'
+    hdr_str_gm_fourier += 'highest theta boundary = ' + str(theta_max_gm) + '\n'
+    hdr_str_gm_fourier += 'number of theta bins = ' + str(ntheta_bins_gm) + '\n'
+    hdr_str_gm_fourier += 'type of binning = ' + str(theta_type_gm) + '\n'
+    hdr_str_gm_fourier += 'ell      W(ell)'
+
+
+    hdr_str_gg_real = 'real space correlation function weights for w in Real space\n'
+    hdr_str_gg_real += 'lowest theta boundary = ' + str(theta_min_gg) + '\n'
+    hdr_str_gg_real += 'highest theta boundary = ' + str(theta_max_gm) + '\n'
+    hdr_str_gg_real += 'number of theta bins = ' + str(ntheta_bins_gm) + '\n'
+    hdr_str_gg_real += 'type of binning = ' + str(theta_type_gm) + '\n'
+    hdr_str_gg_real += 'theta[arcmin]      R(theta)'
+
+    hdr_str_gg_fourier = 'real space correlation function weights for w in Fourier space\n'
+    hdr_str_gg_fourier += 'lowest theta boundary = ' + str(theta_min_gg) + '\n'
+    hdr_str_gg_fourier += 'highest theta boundary = ' + str(theta_max_gg) + '\n'
+    hdr_str_gg_fourier += 'number of theta bins = ' + str(ntheta_bins_gg) + '\n'
+    hdr_str_gg_fourier += 'type of binning = ' + str(theta_type_gg) + '\n'
+    hdr_str_gg_fourier += 'ell      W(ell)'
+
+
     def top_hat(x,width_low, width_high, location):
         return (np.heaviside(x- (location-width_low),1)- np.heaviside(x-(location + width_high),1))/(width_high + width_low)
 
@@ -91,13 +154,13 @@ def main():
             filename_gg = "./../rcf/fourier_weight_realspace_cf_gg_0" + str(i_theta+1) + ".table"
         else:
             filename_gg = "./../rcf/fourier_weight_realspace_cf_gg_" + str(i_theta+1) + ".table"
-        np.savetxt(filename_gg,np.array([fourier_ell,K_gg]).T)
+        np.savetxt(filename_gg,np.array([fourier_ell,K_gg]).T, header = hdr_str_gg_fourier)
         R = top_hat(real_theta, theta_bins[i_theta] - theta_ul_bins[i_theta], theta_ul_bins[i_theta+1] - theta_bins[i_theta],  theta_bins[i_theta])/(real_theta)* (60*180/np.pi)**2/np.sqrt(2)
         if i_theta+1 < 10:
             filename = "./../rcf/real_weight_realspace_cf_gg_0" + str(i_theta+1) + ".table"
         else:
             filename = "./../rcf/real_weight_realspace_cf_gg_" + str(i_theta+1) + ".table"
-        np.savetxt(filename,np.array([real_theta,R]).T)
+        np.savetxt(filename,np.array([real_theta,R]).T, header = hdr_str_gg_real)
 
     # Define theta-range for gamma_t (i.e. gm)
     theta_bins, theta_ul_bins = get_theta_bins(theta_type=theta_type_gm,theta_min = theta_min_gm, theta_max = theta_max_gm, ntheta_bins=ntheta_bins_gm)
@@ -114,13 +177,13 @@ def main():
             filename_gm = "./../rcf/fourier_weight_realspace_cf_gm_0" + str(i_theta+1) + ".table"
         else:
             filename_gm = "./../rcf/fourier_weight_realspace_cf_gm_" + str(i_theta+1) + ".table"
-        np.savetxt(filename_gm,np.array([fourier_ell,K_gm]).T)
+        np.savetxt(filename_gm,np.array([fourier_ell,K_gm]).T, header = hdr_str_gm_fourier)
         R = top_hat(real_theta, theta_bins[i_theta] - theta_ul_bins[i_theta], theta_ul_bins[i_theta+1] - theta_bins[i_theta],  theta_bins[i_theta])/(real_theta)* (60*180/np.pi)**2/np.sqrt(2)
         if i_theta+1 < 10:
             filename = "./../rcf/real_weight_realspace_cf_gm_0" + str(i_theta+1) + ".table"
         else:
             filename = "./../rcf/real_weight_realspace_cf_gm_" + str(i_theta+1) + ".table"
-        np.savetxt(filename,np.array([real_theta,R]).T)
+        np.savetxt(filename,np.array([real_theta,R]).T, header = hdr_str_gm_real)
 
     # Define theta-range for xip_m (i.e. mm)
     theta_bins, theta_ul_bins = get_theta_bins(theta_type=theta_type_mm,theta_min = theta_min_mm, theta_max = theta_max_mm, ntheta_bins=ntheta_bins_mm)
@@ -137,24 +200,24 @@ def main():
             filename_mm = "./../rcf/fourier_weight_realspace_cf_mm_p_0" + str(i_theta+1) + ".table"
         else:
             filename_mm = "./../rcf/fourier_weight_realspace_cf_mm_p_" + str(i_theta+1) + ".table"
-        np.savetxt(filename_mm,np.array([fourier_ell,K_mm]).T)
+        np.savetxt(filename_mm,np.array([fourier_ell,K_mm]).T, header = hdr_str_mm_plus_fourier)
         K_mm = 2/(xu**2 - xl**2)*((xu - 8/xu)*jv(1,xu) - 8*jv(2,xu) - (xl-8/xl)*jv(1,xl) + 8*jv(2,xl))
         if i_theta+1 < 10:
             filename_mm = "./../rcf/fourier_weight_realspace_cf_mm_m_0" + str(i_theta+1) + ".table"
         else:
             filename_mm = "./../rcf/fourier_weight_realspace_cf_mm_m_" + str(i_theta+1) + ".table"
-        np.savetxt(filename_mm,np.array([fourier_ell,K_mm]).T)
+        np.savetxt(filename_mm,np.array([fourier_ell,K_mm]).T, header = hdr_str_mm_minus_fourier)
         R = top_hat(real_theta, theta_bins[i_theta] - theta_ul_bins[i_theta], theta_ul_bins[i_theta+1] - theta_bins[i_theta],  theta_bins[i_theta])/(real_theta)* (60*180/np.pi)**2/np.sqrt(2)
         if i_theta+1 < 10:
             filename = "./../rcf/real_weight_realspace_cf_mm_p_0" + str(i_theta+1) + ".table"
         else:
             filename = "./../rcf/real_weight_realspace_cf_mm_p_" + str(i_theta+1) + ".table"
-        np.savetxt(filename,np.array([real_theta,R]).T)
+        np.savetxt(filename,np.array([real_theta,R]).T, header = hdr_str_mm_plus_real)
         if i_theta+1 < 10:
             filename = "./../rcf/real_weight_realspace_cf_mm_m_0" + str(i_theta+1) + ".table"
         else:
             filename = "./../rcf/real_weight_realspace_cf_mm_m_" + str(i_theta+1) + ".table"
-        np.savetxt(filename,np.array([real_theta,R]).T)
+        np.savetxt(filename,np.array([real_theta,R]).T, header = hdr_str_mm_minus_real)
 
 
 

onecov/cov_arbitrary_summary.py
@@ -1903,7 +1903,7 @@ class CovARBsummary(CovELLSpace):
                 for n_mode in range(self.gg_summaries):
                     inner_integral = np.zeros((len(self.ellrange), flat_length))
                     for i_ell in range(len(self.ellrange)):
-                        self.levin_int_fourier.init_integral(self.ellrange, nongaussELL_flat[:, i_ell, :]*self.ellrange[:, None], True, True)
+                        self.levin_int_fourier.init_integral(self.ellrange, nongaussELL_flat[i_ell, :, :]*self.ellrange[:, None], True, True)
                         inner_integral[i_ell, :] = np.array(self.levin_int_fourier.cquad_integrate_single_well(self.ell_limits[n_mode][:], n_mode))
                     self.levin_int_fourier.init_integral(self.ellrange, inner_integral*self.ellrange[:, None], True, True)
                     nongauss_ASgggg[m_mode, n_mode, :, :, :, :, :, :] = 1.0/(4.0*np.pi**2)*np.reshape(np.array(self.levin_int_fourier.cquad_integrate_single_well(self.ell_limits[m_mode][:], m_mode)),original_shape)
@@ -1935,12 +1935,12 @@ class CovARBsummary(CovELLSpace):
                 for n_mode in range(self.gg_summaries, self.gm_summaries + self.gg_summaries):
                     inner_integral = np.zeros((len(self.ellrange), flat_length))
                     for i_ell in range(len(self.ellrange)):
-                        self.levin_int_fourier.init_integral(self.ellrange, nongaussELL_flat[:, i_ell, :]*self.ellrange[:, None], True, True)
+                        self.levin_int_fourier.init_integral(self.ellrange, nongaussELL_flat[i_ell, :, :]*self.ellrange[:, None], True, True)
                         inner_integral[i_ell, :] = np.array(self.levin_int_fourier.cquad_integrate_single_well(self.ell_limits[n_mode][:], n_mode))
                     self.levin_int_fourier.init_integral(self.ellrange, inner_integral*self.ellrange[:, None], True, True)
                     nongauss_ASgggm[m_mode, n_mode - self.gg_summaries, :, :, :, :, :, :] = 1.0/(4.0*np.pi**2)*np.reshape(np.array(self.levin_int_fourier.cquad_integrate_single_well(self.ell_limits[m_mode][:], m_mode)),original_shape)
                     if connected:
-                        nongauss_ASgggm[m_mode, n_mode, :, :, :, :, :, :] /= (max(survey_params_dict['survey_area_clust'],survey_params_dict['survey_area_ggl']) / self.deg2torad2)
+                        nongauss_ASgggm[m_mode, n_mode - self.gg_summaries, :, :, :, :, :, :] /= (max(survey_params_dict['survey_area_clust'],survey_params_dict['survey_area_ggl']) / self.deg2torad2)
                     eta = (time.time()-t0) / \
                             60 * (tcombs/tcomb-1)
                     print('\rArbitrary summary covariance calculation for the '
@@ -1969,7 +1969,7 @@ class CovARBsummary(CovELLSpace):
                     inner_integralE = np.zeros((len(self.ellrange), flat_length))
                     inner_integralB = np.zeros((len(self.ellrange), flat_length))
                     for i_ell in range(len(self.ellrange)):
-                        self.levin_int_fourier.init_integral(self.ellrange, nongaussELL_flat[:, i_ell, :]*self.ellrange[:, None], True, True)
+                        self.levin_int_fourier.init_integral(self.ellrange, nongaussELL_flat[i_ell, :, :]*self.ellrange[:, None], True, True)
                         inner_integralE[i_ell, :] = np.array(self.levin_int_fourier.cquad_integrate_single_well(self.ell_limits[n_mode][:], n_mode))
                         inner_integralB[i_ell, :] = np.array(self.levin_int_fourier.cquad_integrate_single_well(self.ell_limits[n_mode + self.mmE_summaries][:], n_mode + self.mmE_summaries))
                     self.levin_int_fourier.init_integral(self.ellrange, inner_integralE*self.ellrange[:, None], True, True)
@@ -2006,7 +2006,7 @@ class CovARBsummary(CovELLSpace):
                 for n_mode in range(self.gg_summaries, self.gm_summaries + self.gg_summaries):
                     inner_integral = np.zeros((len(self.ellrange), flat_length))
                     for i_ell in range(len(self.ellrange)):
-                        self.levin_int_fourier.init_integral(self.ellrange, nongaussELL_flat[:, i_ell, :]*self.ellrange[:, None], True, True)
+                        self.levin_int_fourier.init_integral(self.ellrange, nongaussELL_flat[i_ell, :, :]*self.ellrange[:, None], True, True)
                         inner_integral[i_ell, :] = np.array(self.levin_int_fourier.cquad_integrate_single_well(self.ell_limits[n_mode][:], n_mode))
                     self.levin_int_fourier.init_integral(self.ellrange, inner_integral*self.ellrange[:, None], True, True)
                     nongauss_ASgmgm[m_mode - self.gg_summaries, n_mode - self.gg_summaries, :, :, :, :, :, :] = 1.0/(4.0*np.pi**2)*np.reshape(np.array(self.levin_int_fourier.cquad_integrate_single_well(self.ell_limits[m_mode][:], m_mode)),original_shape)
@@ -2040,7 +2040,7 @@ class CovARBsummary(CovELLSpace):
                 for n_mode in range(self.gg_summaries, self.gg_summaries + self.gm_summaries):
                     inner_integral = np.zeros((len(self.ellrange), flat_length))
                     for i_ell in range(len(self.ellrange)):
-                        self.levin_int_fourier.init_integral(self.ellrange, nongaussELL_flat[:, i_ell, :]*self.ellrange[:, None], True, True)
+                        self.levin_int_fourier.init_integral(self.ellrange, nongaussELL_flat[i_ell, :, :]*self.ellrange[:, None], True, True)
                         inner_integral[i_ell, :] = np.array(self.levin_int_fourier.cquad_integrate_single_well(self.ell_limits[n_mode][:], n_mode))
                     self.levin_int_fourier.init_integral(self.ellrange, inner_integral*self.ellrange[:, None], True, True)
                     nongauss_ASEmmgm[m_mode - self.gg_summaries - self.gm_summaries, n_mode - self.gg_summaries, :, :, :, :, :, :] = 1.0/(4.0*np.pi**2)*np.reshape(np.array(self.levin_int_fourier.cquad_integrate_single_well(self.ell_limits[m_mode][:], m_mode)),original_shape)
@@ -2077,7 +2077,7 @@ class CovARBsummary(CovELLSpace):
                     inner_integralE = np.zeros((len(self.ellrange), flat_length))
                     inner_integralB = np.zeros((len(self.ellrange), flat_length))
                     for i_ell in range(len(self.ellrange)):
-                        self.levin_int_fourier.init_integral(self.ellrange, nongaussELL_flat[:, i_ell, :]*self.ellrange[:, None], True, True)
+                        self.levin_int_fourier.init_integral(self.ellrange, nongaussELL_flat[i_ell,:, :]*self.ellrange[:, None], True, True)
                         inner_integralE[i_ell, :] = np.array(self.levin_int_fourier.cquad_integrate_single_well(self.ell_limits[n_mode][:], n_mode))
                         inner_integralB[i_ell, :] = np.array(self.levin_int_fourier.cquad_integrate_single_well(self.ell_limits[n_mode + self.mmE_summaries][:], n_mode + self.mmE_summaries))
                     self.levin_int_fourier.init_integral(self.ellrange, inner_integralE*self.ellrange[:, None], True, True)

onecov/cov_ell_space.py
@@ -232,7 +232,7 @@ class CovELLSpace(PolySpectra):
         if self.redshift_dep_bias:
             self.bias_of_zet = [] 
             for i_tomo in range(self.n_tomo_clust):
-                self.bias_of_zet.append(UnivariateSpline(self.cosmology.comoving_distance(read_in_tables['zclust']['z']).value * self.cosmology.h,read_in_tables['zet_dep_bias'][i_tomo,:],k=3,s=0,ext=0))
+                self.bias_of_zet.append(UnivariateSpline(read_in_tables['zclust']['z'],read_in_tables['zet_dep_bias'][i_tomo,:],k=3,s=0,ext=1))
         self.__set_redshift_distribution_splines(obs_dict['ELLspace'], read_in_tables)
         self.__check_krange_support(
             obs_dict, cosmo_dict, bias_dict, hod_dict, prec)
@@ -5278,7 +5278,7 @@ class CovELLSpace(PolySpectra):
         if self.gg and self.gm and self.cross_terms:
             print("Getting survey modes for gggm")
             ell, sum_m_a_lm = \
-                self.calc_a_lm('gg', 'gm', survey_params_dict)
+                self.calc_a_lm('gm', 'gm', survey_params_dict)
             if ell is not None:
                 ell, sum_m_a_lm = ell[0], sum_m_a_lm[0]
                 x_values = np.zeros((2,len(ell)*len(self.los_integration_chi)))
@@ -5312,7 +5312,7 @@ class CovELLSpace(PolySpectra):
         if self.gg and self.mm and self.cross_terms:
             print("Getting survey modes for ggmm")
             ell, sum_m_a_lm = \
-                self.calc_a_lm('gg', 'mm', survey_params_dict)
+                self.calc_a_lm('gm', 'gm', survey_params_dict)
             if ell is not None:
                 ell, sum_m_a_lm = ell[0], sum_m_a_lm[0]
                 x_values = np.zeros((2,len(ell)*len(self.los_integration_chi)))
@@ -5345,7 +5345,7 @@ class CovELLSpace(PolySpectra):
         if self.gm and self.mm and self.cross_terms:
             print("Getting survey modes for mmgm")
             ell, sum_m_a_lm = \
-                self.calc_a_lm('mm', 'gm', survey_params_dict)
+                self.calc_a_lm('gm', 'gm', survey_params_dict)
             if ell is not None:
                 ell, sum_m_a_lm = ell[0], sum_m_a_lm[0]
                 x_values = np.zeros((2,len(ell)*len(self.los_integration_chi)))
@@ -5432,7 +5432,6 @@ class CovELLSpace(PolySpectra):
                             bias_i_tomo = self.bias_of_zet[i_tomo](self.los_z[i_chi])
                             self.aux_response_gm[i_chi, :, i_sample, i_tomo] = self.aux_response_mm[i_chi, :, i_sample]
                             self.aux_response_gm[i_chi, :, i_sample, i_tomo] -= self.Pgm[:, i_sample]*bias_i_tomo
-                            self.aux_response_gm[i_chi, :, i_sample, i_tomo] *= bias_i_tomo
                 if self.gg:
                     for i_sample in range(self.sample_dim):
                         for i_tomo in range(self.n_tomo_clust):
@@ -5441,7 +5440,6 @@ class CovELLSpace(PolySpectra):
                                 bias_j_tomo = self.bias_of_zet[j_tomo](self.los_z[i_chi])
                                 self.aux_response_gg[i_chi, :, i_sample, i_tomo, j_tomo] = self.aux_response_mm[i_chi, :, i_sample]
                                 self.aux_response_gg[i_chi, :, i_sample, i_tomo, j_tomo] -= (bias_i_tomo + bias_j_tomo)*np.diagonal(self.Pgg, axis1 = -2, axis2 =-1)[:,i_sample]
-                                self.aux_response_gg[i_chi, :, i_sample, i_tomo, j_tomo] *= (bias_i_tomo*bias_j_tomo)
                 eta = (time.time()-t0) * \
                     (len(self.los_z)/(i_chi+1)-1)
                 print('\rPreparations for SSC calculation at '