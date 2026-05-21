update covariance and bug fixes

input/arbitrary_summary/script_weights/get_weights_cosebis.py
@@ -18,7 +18,7 @@ import argparse
 def main():
 
     parser = argparse.ArgumentParser(description='Calculates fourier weights for COSEBIs')
-    parser.add_argument('-n', '--nthread', type=int, default=10, help='number of threads used (default is 10)')
+    parser.add_argument('-n', '--nthread', type=int, default=4, help='number of threads used (default is 4)')
     parser.add_argument('-nf', '--nfourier', type=int, default=int(1e5), help='number of Fourier modes at which the weights are calculated (default is 1e5)')
     parser.add_argument('-nt', '--ntheta', type=int, default=int(1e4), help='number of theta at which the realspace weights are calculated (default is 1e4)')
 
@@ -305,33 +305,37 @@ def main():
             np.savetxt(file_Wn, np.array([ell,result_Well]).T, header=hdr_str_mm_plus_fourier)
         
 
-    Ungg = get_Un(tmax_gg, tmin_gg, theta_gg, Nmax_gg)
-    Ungm = get_Un(tmax_gm, tmin_gm, theta_gm, Nmax_gm)
-    Qngm = get_Qn(Ungm, theta_gm, Nmax_gm)
 
-    for nn in range(1,Nmax_gg+1):
-        Wpsigg = get_Wpsi_ell(nn - 1, Ungg[nn-1,:], theta_gg)
-        if nn < 10:
-            file_Ugg = "./../cosebis/Ugg_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_0"+str(nn)  + ".table"
-            file_Wn = "./../cosebis/Wn_psigg_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
-        else:
-            file_Ugg = "./../cosebis/Ugg_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_"+str(nn)  + ".table"
-            file_Wn = "./../cosebis/Wn_psigg_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
-        np.savetxt(file_Ugg,np.array([theta_gg/arcmintorad,Ungg[nn-1,:]*arcmintorad**2]).T, header=hdr_str_gg_real)
-        if get_W_ell_as_well:
-            np.savetxt(file_Wn, np.array([ell,Wpsigg]).T, header=hdr_str_gg_fourier)
 
-    for nn in range(1,Nmax_gm+1):
-        Wpsigm = get_Wpsi_ell(nn - 1, Ungm[nn-1,:], theta_gm)
-        if nn < 10:
-            file_Qgm = "./../cosebis/Qgm_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_0"+str(nn)  + ".table"
-            file_Wn = "./../cosebis/Wn_psigm_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
-        else:
-            file_Qgm = "./../cosebis/Qgm_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_"+str(nn)  + ".table"
-            file_Wn = "./../cosebis/Wn_psigm_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
-        np.savetxt(file_Qgm,np.array([theta_gm/arcmintorad,Qngm[nn-1,:]*arcmintorad**2]).T, header=hdr_str_gm_real)
-        if get_W_ell_as_well:
-            np.savetxt(file_Wn, np.array([ell,Wpsigm]).T, header=hdr_str_gm_fourier)
+    if Nmax_gg > 0:
+        Ungg = get_Un(tmax_gg, tmin_gg, theta_gg, Nmax_gg)
+    
+        for nn in range(1,Nmax_gg+1):
+            Wpsigg = get_Wpsi_ell(nn - 1, Ungg[nn-1,:], theta_gg)
+            if nn < 10:
+                file_Ugg = "./../cosebis/Ugg_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_0"+str(nn)  + ".table"
+                file_Wn = "./../cosebis/Wn_psigg_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
+            else:
+                file_Ugg = "./../cosebis/Ugg_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_"+str(nn)  + ".table"
+                file_Wn = "./../cosebis/Wn_psigg_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
+            np.savetxt(file_Ugg,np.array([theta_gg/arcmintorad,Ungg[nn-1,:]*arcmintorad**2]).T, header=hdr_str_gg_real)
+            if get_W_ell_as_well:
+                np.savetxt(file_Wn, np.array([ell,Wpsigg]).T, header=hdr_str_gg_fourier)
+
+    if Nmax_gm > 0:
+        Ungm = get_Un(tmax_gm, tmin_gm, theta_gm, Nmax_gm)
+        Qngm = get_Qn(Ungm, theta_gm, Nmax_gm)
+        for nn in range(1,Nmax_gm+1):
+            Wpsigm = get_Wpsi_ell(nn - 1, Ungm[nn-1,:], theta_gm)
+            if nn < 10:
+                file_Qgm = "./../cosebis/Qgm_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_0"+str(nn)  + ".table"
+                file_Wn = "./../cosebis/Wn_psigm_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_0"+str(nn)  + ".table"
+            else:
+                file_Qgm = "./../cosebis/Qgm_" +str(tmin_gg/arcmintorad) + "_to_" + str(tmax_gg/arcmintorad) + "_"+str(nn)  + ".table"
+                file_Wn = "./../cosebis/Wn_psigm_"  +str(tmin_mm/arcmintorad) + "_to_" + str(tmax_mm/arcmintorad) + "_"+str(nn)  + ".table"
+            np.savetxt(file_Qgm,np.array([theta_gm/arcmintorad,Qngm[nn-1,:]*arcmintorad**2]).T, header=hdr_str_gm_real)
+            if get_W_ell_as_well:
+                np.savetxt(file_Wn, np.array([ell,Wpsigm]).T, header=hdr_str_gm_fourier)
 
 if __name__ == '__main__':
     main()

onecov/cov_polyspectra.py
@@ -1111,71 +1111,56 @@ class PolySpectra(HaloModel):
                                  tri_bool)
 
         k_dim = len(self.mass_func.k)
+        b_i = self.effective_bias[None, None, :, None]
+        b_j = self.effective_bias[None, None, None, :]
+        b_i2 = b_i ** 2
+        tri234 = trispectra234h[:, :, None, None]
 
         if self.gg and tri_gggg:
-            if len(np.where(trispec1h_gggg + trispectra234h[:, :, None, None]*self.effective_bias[None,None,:,None]**2*self.effective_bias[None,None,None, :]**2 <0)[0]) == 0:
-                logtrispec_gggg_ktri = \
-                    np.log10(trispec1h_gggg + trispectra234h[:, :, None, None]*self.effective_bias[None,None,:,None]**2*self.effective_bias[None,None,None, :]**2)
-            else:
-                logtrispec_gggg_ktri = trispec1h_gggg + trispectra234h[:, :, None, None]*self.effective_bias[None,None,:,None]**2*self.effective_bias[None,None,None, :]**2
+            combined = trispec1h_gggg + tri234 * b_i2 * b_j**2
+            logtrispec_gggg_ktri = combined if np.any(combined < 0) else np.log10(combined)
             trispec_gggg = np.zeros((k_dim, k_dim,
                                      self.sample_dim, self.sample_dim))
         else:
             logtrispec_gggg_ktri = None
 
         if self.gg and self.gm and self.cross_terms and tri_gggm:
-            if len(np.where(trispec1h_gggm + trispectra234h[:, :, None, None]*self.effective_bias[None,None,:,None]**2*self.effective_bias[None,None,None, :] <0)[0]) == 0:
-                logtrispec_gggm_ktri = \
-                    np.log10(trispec1h_gggm + trispectra234h[:, :, None, None]*self.effective_bias[None,None,:,None]**2*self.effective_bias[None,None,None, :])
-            else:
-                logtrispec_gggm_ktri = \
-                    (trispec1h_gggm + trispectra234h[:, :, None, None]*self.effective_bias[None,None,:,None]**2*self.effective_bias[None,None,None, :])
+            combined = trispec1h_gggm + tri234 * b_i2 * b_j
+            logtrispec_gggm_ktri = combined if np.any(combined < 0) else np.log10(combined)
             trispec_gggm = np.zeros((k_dim, k_dim,
                                      self.sample_dim, self.sample_dim))
         else:
             logtrispec_gggm_ktri = None
 
         if self.gg and self.mm and self.cross_terms and tri_ggmm:
-            if len(np.where(trispec1h_ggmm + trispectra234h[:, :, None, None]*self.effective_bias[None,None,:,None]**2 < 0)[0]) == 0:
-                logtrispec_ggmm_ktri = \
-                    np.log10(trispec1h_ggmm + trispectra234h[:, :, None, None]*self.effective_bias[None,None,:,None]**2)
-            else:
-                logtrispec_ggmm_ktri = trispec1h_ggmm + trispectra234h[:, :, None, None]*self.effective_bias[None,None,:,None]**2
+            combined = trispec1h_ggmm + tri234 * b_i2
+            logtrispec_ggmm_ktri = combined if np.any(combined < 0) else np.log10(combined)
             trispec_ggmm = np.zeros((k_dim, k_dim,
                                      self.sample_dim, self.sample_dim))
         else:
             logtrispec_ggmm_ktri = None
 
         if self.gm and tri_gmgm:
-            if len(np.where(trispec1h_gmgm + trispectra234h[:, :, None, None]*self.effective_bias[None,None,:,None]*self.effective_bias[None,None,None, :] < 0)[0]) == 0:
-                logtrispec_gmgm_ktri = \
-                    np.log10(trispec1h_gmgm + trispectra234h[:, :, None, None]*self.effective_bias[None,None,:,None]*self.effective_bias[None,None,None, :])
-            else:
-                logtrispec_gmgm_ktri = trispec1h_gmgm + trispectra234h[:, :, None, None]*self.effective_bias[None,None,:,None]*self.effective_bias[None,None,None, :]
+            combined = trispec1h_gmgm + tri234 * b_i * b_j
+            logtrispec_gmgm_ktri = combined if np.any(combined < 0) else np.log10(combined)
             trispec_gmgm = np.zeros((k_dim, k_dim,
                                      self.sample_dim, self.sample_dim))
         else:
             logtrispec_gmgm_ktri = None
 
         if self.mm and self.gm and self.cross_terms and tri_mmgm:
-            if len(np.where(trispec1h_mmgm + trispectra234h[:, :, None, None]*self.effective_bias[None,None,None,:] < 0)[0]) == 0:
-                logtrispec_mmgm_ktri = \
-                    np.log10(trispec1h_mmgm + trispectra234h[:, :, None, None]*self.effective_bias[None,None,None,:])
-            else:
-                logtrispec_mmgm_ktri = trispec1h_mmgm + trispectra234h[:, :, None, None]*self.effective_bias[None,None,None,:]
+            combined = trispec1h_mmgm + tri234 * b_j
+            logtrispec_mmgm_ktri = combined if np.any(combined < 0) else np.log10(combined)
             trispec_mmgm = np.zeros((k_dim, k_dim,
                                      self.sample_dim, self.sample_dim))
         else:
             logtrispec_mmgm_ktri = None
+
         if self.mm and tri_mmmm:
-            if len(np.where(trispec1h_mmmm + trispectra234h[:, :, None, None] <0)[0]) == 0:
-                logtrispec_mmmm_ktri = \
-                    np.log10(trispec1h_mmmm + trispectra234h[:, :, None, None])
-            else:
-                logtrispec_mmmm_ktri = trispec1h_mmmm + trispectra234h[:, :, None, None]
+            combined = trispec1h_mmmm + tri234
+            logtrispec_mmmm_ktri = combined if np.any(combined < 0) else np.log10(combined)
             trispec_mmmm = np.zeros((k_dim, k_dim,
                                      self.sample_dim, self.sample_dim))
-            
         else:
             logtrispec_mmmm_ktri = None
 
@@ -1257,58 +1242,55 @@ class PolySpectra(HaloModel):
         
         '''
         
-        x_values = np.zeros((2,len(self.mass_func.k)*len(self.mass_func.k)))
-        flat_idx = 0
-        for i_k in range(len(self.mass_func.k)):
-            for j_k in range(len(self.mass_func.k)):
-                x_values[0,flat_idx] = np.log10(self.mass_func.k[i_k])
-                x_values[1,flat_idx] = np.log10(self.mass_func.k[j_k])
-                flat_idx +=1
+        logk = np.log10(self.mass_func.k)
+        kk_i, kk_j = np.meshgrid(logk, logk, indexing='ij')
+        x_values = np.stack([kk_i.ravel(), kk_j.ravel()])
+        xi = x_values.T 
         np.seterr(over='ignore')
         for nbin in range(self.sample_dim):
             for mbin in range(nbin, self.sample_dim):
                 if self.gg and tri_gggg:
                     tri_interpolator = RegularGridInterpolator((self.logks_tri, self.logks_tri),
-                                                    logtrispec_gggg_ktri,bounds_error= False, fill_value = None)
-                    if np.isinf(10**np.max(logtrispec_gggg_ktri)):
-                        trispec_gggg[:,:,nbin,mbin] = tri_interpolator((x_values[0,:],x_values[1,:])).reshape((len(self.mass_func.k),len(self.mass_func.k)))
+                                                    logtrispec_gggg_ktri[:,:,nbin,mbin],bounds_error= False, fill_value = None)
+                    if np.max(logtrispec_gggg_ktri[:,:,nbin,mbin]) > 308.255:
+                        trispec_gggg[:,:,nbin,mbin] = tri_interpolator(xi).reshape((len(self.mass_func.k),len(self.mass_func.k)))
                     else:
-                        trispec_gggg[:,:,nbin,mbin] = 10**tri_interpolator((x_values[0,:],x_values[1,:])).reshape((len(self.mass_func.k),len(self.mass_func.k)))
+                        trispec_gggg[:,:,nbin,mbin] = 10**tri_interpolator(xi).reshape((len(self.mass_func.k),len(self.mass_func.k)))
                 if self.gg and self.gm and self.cross_terms:
                     tri_interpolator = RegularGridInterpolator((self.logks_tri, self.logks_tri),
-                                                    logtrispec_gggm_ktri,bounds_error= False, fill_value = None)
-                    if np.isinf(10**np.max(logtrispec_gggm_ktri)):
-                        trispec_gggm[:,:,nbin,mbin] = tri_interpolator((x_values[0,:],x_values[1,:])).reshape((len(self.mass_func.k),len(self.mass_func.k)))
-                    else: 
-                        trispec_gggm[:,:,nbin,mbin] = 10**tri_interpolator((x_values[0,:],x_values[1,:])).reshape((len(self.mass_func.k),len(self.mass_func.k)))
+                                                    logtrispec_gggm_ktri[:,:,nbin,mbin],bounds_error= False, fill_value = None)
+                    if np.max(logtrispec_gggm_ktri[:,:,nbin,mbin]) > 308.255:
+                        trispec_gggm[:,:,nbin,mbin] = tri_interpolator(xi).reshape((len(self.mass_func.k),len(self.mass_func.k)))
+                    else:
+                        trispec_gggm[:,:,nbin,mbin] = 10**tri_interpolator(xi).reshape((len(self.mass_func.k),len(self.mass_func.k)))
                 if self.gg and self.mm and self.cross_terms:
                     tri_interpolator = RegularGridInterpolator((self.logks_tri, self.logks_tri),
-                                                    logtrispec_ggmm_ktri,bounds_error= False, fill_value = None)
-                    if np.isinf(10**np.max(logtrispec_ggmm_ktri)):
-                        trispec_ggmm[:,:,nbin,mbin] = tri_interpolator((x_values[0,:],x_values[1,:])).reshape((len(self.mass_func.k),len(self.mass_func.k)))
+                                                    logtrispec_ggmm_ktri[:,:,nbin,mbin],bounds_error= False, fill_value = None)
+                    if np.max(logtrispec_ggmm_ktri[:,:,nbin,mbin]) > 308.255:
+                        trispec_ggmm[:,:,nbin,mbin] = tri_interpolator(xi).reshape((len(self.mass_func.k),len(self.mass_func.k)))
                     else:
-                        trispec_ggmm[:,:,nbin,mbin] = 10**tri_interpolator((x_values[0,:],x_values[1,:])).reshape((len(self.mass_func.k),len(self.mass_func.k)))
+                        trispec_ggmm[:,:,nbin,mbin] = 10**tri_interpolator(xi).reshape((len(self.mass_func.k),len(self.mass_func.k)))
                 if self.gm:
                     tri_interpolator = RegularGridInterpolator((self.logks_tri, self.logks_tri),
-                                                    logtrispec_gmgm_ktri,bounds_error= False, fill_value = None)
-                    if np.isinf(10**np.max(logtrispec_gmgm_ktri)):
-                        trispec_gmgm[:,:,nbin,mbin] = tri_interpolator((x_values[0,:],x_values[1,:])).reshape((len(self.mass_func.k),len(self.mass_func.k)))
+                                                    logtrispec_gmgm_ktri[:,:,nbin,mbin],bounds_error= False, fill_value = None)
+                    if np.max(logtrispec_gmgm_ktri[:,:,nbin,mbin]) > 308.255:
+                        trispec_gmgm[:,:,nbin,mbin] = tri_interpolator(xi).reshape((len(self.mass_func.k),len(self.mass_func.k)))
                     else:
-                        trispec_gmgm[:,:,nbin,mbin] = 10**tri_interpolator((x_values[0,:],x_values[1,:])).reshape((len(self.mass_func.k),len(self.mass_func.k)))
+                        trispec_gmgm[:,:,nbin,mbin] = 10**tri_interpolator(xi).reshape((len(self.mass_func.k),len(self.mass_func.k)))
                 if self.gm and self.mm and self.cross_terms:
                     tri_interpolator = RegularGridInterpolator((self.logks_tri, self.logks_tri),
-                                                    logtrispec_mmgm_ktri,bounds_error= False, fill_value = None)
-                    if np.isinf(10**np.max(logtrispec_mmgm_ktri)):
-                        trispec_mmgm[:,:,nbin,mbin] = tri_interpolator((x_values[0,:],x_values[1,:])).reshape((len(self.mass_func.k),len(self.mass_func.k)))
+                                                    logtrispec_mmgm_ktri[:,:,nbin,mbin],bounds_error= False, fill_value = None)
+                    if np.max(logtrispec_mmgm_ktri[:,:,nbin,mbin]) > 308.255:
+                        trispec_mmgm[:,:,nbin,mbin] = tri_interpolator(xi).reshape((len(self.mass_func.k),len(self.mass_func.k)))
                     else:
-                        trispec_mmgm[:,:,nbin,mbin] = 10**tri_interpolator((x_values[0,:],x_values[1,:])).reshape((len(self.mass_func.k),len(self.mass_func.k)))
+                        trispec_mmgm[:,:,nbin,mbin] = 10**tri_interpolator(xi).reshape((len(self.mass_func.k),len(self.mass_func.k)))
                 if self.mm:
                     tri_interpolator = RegularGridInterpolator((self.logks_tri, self.logks_tri),
-                                                    logtrispec_mmmm_ktri,bounds_error= False, fill_value = None)
-                    if np.isinf(10**np.max(logtrispec_mmmm_ktri)):
-                        trispec_mmmm[:,:,nbin,mbin] = tri_interpolator((x_values[0,:],x_values[1,:])).reshape((len(self.mass_func.k),len(self.mass_func.k)))
+                                                    logtrispec_mmmm_ktri[:,:,nbin,mbin],bounds_error= False, fill_value = None)
+                    if np.max(logtrispec_mmmm_ktri[:,:,nbin,mbin]) > 308.255:
+                        trispec_mmmm[:,:,nbin,mbin] = tri_interpolator(xi).reshape((len(self.mass_func.k),len(self.mass_func.k)))
                     else:
-                        trispec_mmmm[:,:,nbin,mbin] = 10**tri_interpolator((x_values[0,:],x_values[1,:])).reshape((len(self.mass_func.k),len(self.mass_func.k)))
+                        trispec_mmmm[:,:,nbin,mbin] = 10**tri_interpolator(xi).reshape((len(self.mass_func.k),len(self.mass_func.k)))
         np.seterr(over='warn')
         if self.gg:
             trispec_gggg[np.where(trispec_gggg < self.tri_lowlim)] = \
@@ -1909,7 +1891,7 @@ class PolySpectra(HaloModel):
         integral_mmm = \
             10**halomod_integral_mmm_spline(self.logks_tri, self.logks_tri)
 
-        phis = np.linspace(0, np.pi/2, 300)
+        phis = np.linspace(0, np.pi/2, 3000)
         kidxlist = \
             list(itertools.combinations_with_replacement(np.arange(k_dim), 2))
 